acffcb7dd2fa0342217253b8e1387c7949f890ff
Ivana Arabadzic <ivana.arabadzic.htl@gmail.com>
2025-11-27 14:00:20 +0100

    Test für Server in Program.cs

--- MRP/Program.cs
+++ MRP/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Net;
+using MRP.Server;
 
-Console.WriteLine("Hello, World!");
+//kurzes test program.cs um zu sehen ob der server funktioniert bisher
+var server = new HttpRestServer();
+
+server.RequestReceived += (HttpListenerContext ctx) =>
+{
+    string message = "Hello World!";
+    byte[] buffer = System.Text.Encoding.UTF8.GetBytes(message);
+    
+    ctx.Response.StatusCode = 200;
+    ctx.Response.ContentType = "text/plain";
+    ctx.Response.OutputStream.Write(buffer, 0, buffer.Length);
+    ctx.Response.Close();
+
+};
+
+server.Run();
